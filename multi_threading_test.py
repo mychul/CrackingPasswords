ebf90c93b8e55fcaf10b7f17c7c21d10f013d712
TinhLa72 <52975703+TinhLa72@users.noreply.github.com>
2021-01-13 18:42:57 -0800

    Update multi_threading_test.py

--- multi_threading_test.py
+++ multi_threading_test.py
@@ -1,23 +1,26 @@
 from multiprocessing import Process, Value
 
 def x(num):
-    for i in range(4):
+    for i in range(10):
         num.value = num.value + 1
 
-def y(num):
-    for i in range(4):
-        num.value = num.value + 1
+#def y(num):
+#    for i in range(10):
+#        num.value = num.value + 1
 
 if __name__ == '__main__':
     num = Value('i', 0)
 
     x_process = Process(target=x, args=(num,))
-    y_process = Process(target=y, args=(num,))
+    y_process = Process(target=x, args=(num,))
+    z_process = Process(target=x, args=(num,))
 
     x_process.start()
     y_process.start()
+    z_process.start()
 
     x_process.join()
     y_process.join()
+    z_process.join()
 
     print(num.value)
