7532a7b11af47de71ec38f65404332af9e52aa1b
TinhLa72 <52975703+TinhLa72@users.noreply.github.com>
2021-01-13 16:13:40 -0800

    Update time_program.py

--- time_program.py
+++ time_program.py
@@ -1,7 +1,6 @@
 import time
+
 start = time.time()
-tmp = input("Press Anything: ") #Program runs here
+test = input("Type Something: ")
 end = time.time()
-start_milliseconds = int(start * 1000)
-end_milliseconds = int(end * 1000)
-print(str(end_milliseconds - start_milliseconds) + " ms")
+print(str(end - start) + " seconds")
